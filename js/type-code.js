0daf3a4aa6f52b7daf4b1bfbdb01e5a3751e81d5
ramz <ramz.13m@mail.ru>
2022-05-12 17:11:25 +0500

    xatolar to'g'irlandi

---
+++ js/type-code.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.text-type', {
+  strings: [
+  'Frontend',
+  'Developer',
+  'Freelancer'
+  ],
+  showCursor : false,
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop : true
+});
