89fa26cb44a9844d41a7f0fdd16f9f6426cbf744
ramz <ramz.13m@mail.ru>
2022-05-13 03:42:27 +0500

    add js modal style

--- js/app.js
+++ js/app.js
@@ -3,19 +3,41 @@ document.addEventListener("DOMContentLoaded", () => {
   const chk = document.getElementById('chk');
   const headerButton = document.querySelector(".header__open");
   const headerList = document.querySelector(".header__list");
-  const closeButton = document.querySelector(".header__close-button")
+  const closeButton = document.querySelector(".header__close-button");
+  const box = document.querySelector(".header__info-box")
   
   chk.addEventListener('click', () => {
     document.body.classList.toggle('header__dark');
   });
 
   headerButton.addEventListener('click', () => {
-    headerList.classList.add("header__toggle-modal")
+    headerList.classList.add("header__toggle-modal");
+    document.body.style.overflow = "hidden";
   })
   
   closeButton.addEventListener('click', ()=>{
-    headerList.classList.remove("header__toggle-modal")
+    headerList.classList.remove("header__toggle-modal");
+    document.body.style.overflow = "scroll";
+    
   })
+  
+  document.addEventListener("keydown", (e)=>{
+    if(e.code === "Escape"){
+      headerList.classList.remove("header__toggle-modal");
+      // modalSection.style.display = "none"
+      document.body.style.overflow = ""
+    }
+  })
+  
+  box.addEventListener("click", ()=>{
+    headerList.classList.remove("header__toggle-modal");
+  })
+
+  
+  
+  // 
+  
+  
 
 
   // vanilla
